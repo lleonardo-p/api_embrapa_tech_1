f975ebd933d8143f89884bbbb6588562bae323cf
lleonardo-p <lleonardo_pereira@outlook.com>
2024-11-03 18:32:06 -0300

    Update Python version

--- api_embrapa_tech_1/api/main.py
+++ api_embrapa_tech_1/api/main.py
@@ -20,5 +20,5 @@ async def custom_404_handler(request, exc):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
 
